490bb582a7a328ec5cb028d6096d90779fe9bb3c
Enrique Salazar <xeast98@gmail.com>
2019-11-06 07:02:48 -0430

    Update Enrique_27000054.py

--- Enrique_27000054.py
+++ Enrique_27000054.py
@@ -5,7 +5,7 @@ Proyecto Algoritmo de Newton-Raphson.
 Cada participante debe completar su módulo y luego solicitar el Pull-Request.
 """
 
-import math
+from math import *
 
 def derivada(f, h = 0.02):
     """
@@ -21,23 +21,39 @@ def derivada(f, h = 0.02):
 
     return _
 
-def newton_raphson(f, x, ER, N):
-    """
-    Implementa el Algoritmo de Newton-Raphson y retorna la aproximación de la
-    raiz.
-
-    Parámetros:
-    f: función de variable real f(x).
-    x: aproximación inicial.
-    ER: cota mínima del error relativo.
-    N: número máximo de iteraciones.
-    """
-
-    print("Iteración:", i, "Aproximación:", xi, "Error:", err)
-
+def newton_raphson(f,x,ER,N):
+    df=derivada(f, h=0.02)
+    xi= x
+    error= 1e3
+    n=1
+    while (error>ER) and (n<N):
+        xi= xi-f(xi)/df(xi)
+        error = abs (f(xi))
+        print ("Iteracion :", (n)," Aproximacion: ", (xi), " Error: ", (error))
+        n= n+1 
+    print ("Solucion aproximada ", (xi))
     return xi
+
+##def newton_raphson(f, x, ER, N):
+##    """
+##    Implementa el Algoritmo de Newton-Raphson y retorna la aproximación de la
+ ##   raiz.
+
+##    Parámetros:
+##    f: función de variable real f(x).
+##    x: aproximación inicial.
+##    ER: cota mínima del error relativo.
+##    N: número máximo de iteraciones.
+##    """
+
+##    print("Iteración:", i, "Aproximación:", xi, "Error:", err)
+
+##   return xi
 
 
 if __name__ == "__main__":
     # Pruebe aquí su función.
+    f= lambda x: x**2-10;
+    newton_raphson(f,3,1e-3,5)
     pass
+
